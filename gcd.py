c1029d24b86971385edc79298f63b210d4e80d8e
Jonathan Elliston <68207187+jonathanElliston@users.noreply.github.com>
2020-07-12 19:40:44 -0700

    Update gcd.py

--- gcd.py
+++ gcd.py
@@ -1,4 +1,5 @@
 def gcd(a, b):
+    #calculates the gcd of two integers using the Euclidean algorithm
     if a < b:
         a, b = b, a
     if b == 0:
@@ -6,11 +7,13 @@ def gcd(a, b):
     return gcd(b, a%b)
     
 def lcm(a,b):
+    #calculates lcm of two integers
     if a < b:
         a, b = b, a
     return int(a/gcd(a,b)*b)
 
-def addIntFractions(num1, den1, num2, den2):
+def addFractions(num1, den1, num2, den2):
+    #adds two fractions and reduces the result
     num3 = num1*den2 + num2*den1
     den3 = den1*den2
     return int(num3/(gcd(num3, den3))), int(den3/(gcd(num3, den3)))
